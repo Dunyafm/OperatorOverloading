34f4a06a2912d6ce22d0fd770e0b43fd5ca22468
unknown <Dunyafm@code.edu.az>
2021-12-13 15:17:03 -0800

    Personabout

---
+++ Personabout/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Personabout
+{
+    class Program
+    {
+        
+
+       public static void Main(string[] args)
+       {
+
+            Listener listener = new Listener("Leyla", "Musayeva" ,23);
+            Student student = new Student("Ali", "Rzayev" ,25);
+
+            Console.WriteLine(listener.Age > student.Age);
+            
+
+       }
+
+    }
+   
+}
+
+class Student
+{
+    public string Name { get; set; }
+    public String Surname { get; set; }
+    public int Age { get; set; }
+
+    public Student(string name, string surname, int age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+
+    }
+}
+
+    public static bool operator > (Listener listener, Student student)
+    {
+
+        return listener.Age > student.Age;
+    }
+    public static bool operator < (Listener listener,Student student)
+    {
+
+            return listener.Age  < student.Age;
+    }
+
+        class Listener
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int Age { get; set; }
+
+            public Listener(string name, string surname, int age)
+            {
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
